6ceff5423b3aff88d1f18cf6f9857eb54fa30062
K-Doshita <kdoush.haru@gmail.com>
2022-05-31 23:58:36 +0900

    add: 物理挙動テスト追加

---
+++ PhysicsTest/Assets/PhysicsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsTest : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 vector3 = new Vector3(0, 100f, 0);
+
+        //rigidbody.AddForce(transform.up * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rigidbody.AddForce(transform.up * 7500);
+        } 
+    }
+}
